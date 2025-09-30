e77457a839925d90e587485315c54aab63d5c158
Aiman <aimanahmed61@gmail.com>
2025-09-30 10:08:22 +0100

    EDIT: implemented main trick and tested game in different file

--- extra_function_frenzy.py
+++ extra_function_frenzy.py
@@ -3,3 +3,8 @@ def is_number_cool(num: int) -> bool:
         return False
     else:
         return True
+
+
+
+import rps
+rps.main()
--- rps.py
+++ rps.py
@@ -1,34 +1,36 @@
 import random
 import rps_functions
 
-player_win_count = 0
-computerWinCount = 0
+def main():
+    player_win_count = 0
+    computerWinCount = 0
 
 
-print("Welcome top rock paper scissors game!")
+    print("Welcome top rock paper scissors game!")
 
-winning_score = 2
-while player_win_count < winning_score and computerWinCount < winning_score:
+    winning_score = 2
+    while player_win_count < winning_score and computerWinCount < winning_score:
 
-    player_input = get_user_input()
-    com_turn = get_computer_choice()
+        player_input = rps_functions.get_user_input()
+        com_turn = rps_functions.get_computer_choice()
 
-    print("The computer went: " + com_turn)
+        print("The computer went: " + com_turn)
 
+        player_outcome = rps_functions.get_outcome(player_input,com_turn)
 
+        if player_outcome == "w":
+            print("You won this round!")
+            player_win_count+=1
+        elif player_outcome == "d":
+            print("It's a draw!")
+        else:
+            print("You lost this round!")
+            computerWinCount+=1
 
-    player_outcome = get_outcome(player_input,com_turn)
-
-    if player_outcome == "w":
-        print("You won this round!")
-        player_win_count+=1
-    elif player_outcome == "d":
-        print("It's a draw!")
+    if player_win_count == winning_score:
+        print("You Won!!!!!")
     else:
-        print("You lost this round!")
-        computerWinCount+=1
+        print("Computer Won!")
 
-if player_win_count == winning_score:
-    print("You Won!!!!!")
-else:
-    print("Computer Won!")
+if __name__ == "__main__":
+    main()
